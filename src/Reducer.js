d244fc7d75bdd26f636d10f751688540b9ab5255
mjsalinas <mariasalinas713@gmail.com>
2022-07-16 01:32:36 -0600

    returning state in reducer

--- src/Reducer.js
+++ src/Reducer.js
@@ -12,18 +12,23 @@ export const UserSlice = createSlice({
   reducers: {
     setUser: (state, action) => {
       state.user = action.payload;
+      return state;
     },
     setAsesorias: (state, action) => {
       state.asesorias = action.payload;
+      return state;
     },
     setSelectedAsesoria: (state, action) => {
       state.selectedAsesoria = action.payload;
+      return state;
     },
     setAsesoriasPendientes: (state, action) => {
       state.asesoriasPendientes = action.payload;
+      return state;
     },
     setAsesoriasCompletas: (state, action) => {
       state.asesoriasCompletas = action.payload;
+      return state;
     },
   },
 });
